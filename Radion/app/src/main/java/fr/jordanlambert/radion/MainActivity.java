a74e6832711f4caad8b09196a107adb421b9f2a7
Jordan Lambert <lambert.jord@gmail.com>
2017-01-26 15:01:20 +0100

    [Misc] I'm back
    Dunno what i’ve to put on title. Too many things, bad idea to commit
    after that but i’ve to do

--- Radion/app/src/main/java/fr/jordanlambert/radion/MainActivity.java
+++ Radion/app/src/main/java/fr/jordanlambert/radion/MainActivity.java
@@ -1,8 +1,12 @@
 package fr.jordanlambert.radion;
 
+import android.app.SearchManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.SearchView;
+import android.view.MenuInflater;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,6 +16,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -23,15 +28,6 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -40,6 +36,7 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
     }
 
     @Override
@@ -56,6 +53,11 @@ public class MainActivity extends AppCompatActivity
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
+
+        // Retrieve the SearchView and plug it into SearchManager
+        final SearchView searchView = (SearchView) MenuItemCompat.getActionView(menu.findItem(R.id.action_search));
+        SearchManager searchManager = (SearchManager) getSystemService(SEARCH_SERVICE);
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
         return true;
     }
 
@@ -80,13 +82,25 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        TextView t;
+        t=new TextView(this);
 
-        } else if (id == R.id.nav_slideshow) {
+        t=(TextView)findViewById(R.id.test);
 
-        } else if (id == R.id.nav_manage) {
+        if (id == R.id.nav_games) {
+            t.setText("Games sections");
+
+        } else if (id == R.id.nav_stations) {
+            t.setText("Stations sections");
+
+        } else if (id == R.id.nav_discover) {
+            t.setText("Discover sections");
+
+        } else if (id == R.id.nav_news) {
+            t.setText("News sections");
+
+        } else if (id == R.id.nav_settings) {
+            t.setText("Settings sections");
 
         } else if (id == R.id.nav_share) {
 
