f504047239ebd9689803924b04a6e6fec60036a9
Jordan Lambert <lambert.jord@gmail.com>
2017-01-26 19:56:32 +0100

    [WIP] API call

--- Radion/app/src/main/java/fr/jordanlambert/radion/MainActivity.java
+++ Radion/app/src/main/java/fr/jordanlambert/radion/MainActivity.java
@@ -1,30 +1,60 @@
 package fr.jordanlambert.radion;
 
 import android.app.SearchManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v4.view.MenuItemCompat;
-import android.support.v7.widget.SearchView;
-import android.view.MenuInflater;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import static fr.jordanlambert.radion.MainActivity.API_URL;
+import static fr.jordanlambert.radion.R.id.progressBar;
+import static fr.jordanlambert.radion.R.id.responseView;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    TextView responseView;
+    ProgressBar progressBar;
+    static final String API_KEY = "BXn7Z6v2KVmshpwalMHuNQrMIijEp1T9lvDjsnC0gyvfhLdIYU";
+    static final String API_URL = "https://igdbcom-internet-game-database-v1.p.mashape.com/games/?fields=*&limit=3&offset=0&order=release_dates.date:desc&search=zelda";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Declaration
+        responseView = (TextView) findViewById(R.id.responseView);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+
+        Button queryButton = (Button) findViewById(R.id.queryButton);
+        queryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new RetrieveFeedTask().execute();
+            }
+        });
+
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -113,3 +143,65 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 }
+
+class RetrieveFeedTask extends AsyncTask<Void, Void, String> {
+
+    private Exception exception;
+
+    protected void onPreExecute() {
+
+        progressBar.setVisibility(View.VISIBLE);
+        responseView.setText("text");
+    }
+
+    protected String doInBackground(Void... urls) {
+        // Do some validation here
+
+        try {
+            URL url = new URL(API_URL);
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            try {
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                StringBuilder stringBuilder = new StringBuilder();
+                String line;
+                while ((line = bufferedReader.readLine()) != null) {
+                    stringBuilder.append(line).append("\n");
+                }
+                bufferedReader.close();
+                return stringBuilder.toString();
+            }
+            finally{
+                urlConnection.disconnect();
+            }
+        }
+        catch(Exception e) {
+            Log.e("ERROR", e.getMessage(), e);
+            return null;
+        }
+    }
+
+    protected void onPostExecute(String response) {
+        if(response == null) {
+            response = "THERE WAS AN ERROR";
+        }
+        progressBar.setVisibility(View.GONE);
+        Log.i("INFO", response);
+        responseView.setText(response);
+        // TODO: check this.exception
+        // TODO: do something with the feed
+
+//            try {
+//                JSONObject object = (JSONObject) new JSONTokener(response).nextValue();
+//                String requestID = object.getString("requestId");
+//                int likelihood = object.getInt("likelihood");
+//                JSONArray photos = object.getJSONArray("photos");
+//                .
+//                .
+//                .
+//                .
+//            } catch (JSONException e) {
+//                e.printStackTrace();
+//            }
+    }
+}
+
