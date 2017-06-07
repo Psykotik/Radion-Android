a8526bb606125c8cc377b53e3207ac3aa988307e
Psykotik <lambert.jord@gmail.com>
2017-06-07 16:36:44 +0200

    [Add] Fragments UX
    Now fragments are accessible and all cells on nav menu are available

--- Radion/app/src/main/java/fr/jordanlambert/radion/activity/MainActivity.java
+++ Radion/app/src/main/java/fr/jordanlambert/radion/activity/MainActivity.java
@@ -1,10 +1,17 @@
 package fr.jordanlambert.radion.activity;
 
 import android.app.SearchManager;
+import android.content.Intent;
 import android.graphics.PorterDuff;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -17,8 +24,10 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -30,18 +39,51 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 import fr.jordanlambert.radion.R;
+import fr.jordanlambert.radion.fragment.DiscoverFragment;
+import fr.jordanlambert.radion.fragment.GamesFragment;
+import fr.jordanlambert.radion.fragment.NewsFragment;
+import fr.jordanlambert.radion.fragment.SettingsFragment;
+import fr.jordanlambert.radion.fragment.StationsFragment;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener,
+        GamesFragment.OnFragmentInteractionListener,
+        DiscoverFragment.OnFragmentInteractionListener,
+        NewsFragment.OnFragmentInteractionListener,
+        SettingsFragment.OnFragmentInteractionListener,
+        StationsFragment.OnFragmentInteractionListener{
+
+    // index to identify current nav menu item
+    public static int navItemIndex = 0;
+
+    // tags used to attach the fragments
+    private static final String TAG_GAMES = "games";
+    private static final String TAG_STATIONS = "stations";
+    private static final String TAG_DISCOVER = "discover";
+    private static final String TAG_NEWS = "news";
+    private static final String TAG_SETTINGS = "settings";
+    public static String CURRENT_TAG = TAG_GAMES;
 
     private static final String TAG = "MainActivity";
 
+    // toolbar titles respected to selected nav menu item
+    private String[] activityTitles;
+
+    // flag to load home fragment when user presses back key
+    private boolean shouldLoadHomeFragOnBackPress = true;
+    private Handler mHandler;
+
+
+    private NavigationView navigationView;
+    private DrawerLayout drawer;
+    private View navHeader;
+    private ImageView imgNavHeaderBg, imgProfile;
+    private TextView txtName, txtWebsite;
+    private Toolbar toolbar;
+    private FloatingActionButton fab;
     private TextView responseView;
     private ProgressBar progressBar;
-    /*
-        static final String API_KEY = "343593b07c667d6";
-        static final String API_URL = "https://api.fullcontact.com/v2/person.json?";
-    */
+
     private static final String API_KEY = "lwUWLTDhm1mshKNpTegeITNu8qlVp1puJaGjsnlsx0Jnlkb1X3";
     private static final String API_URL = "https://igdbcom-internet-game-database-v1.p.mashape.com/games/?fields=name%2Csummary%2Cslug%2Curl&limit=10&offset=0&order=release_dates.date%3Adesc&search=zelda";
 
@@ -53,8 +95,42 @@ public class MainActivity extends AppCompatActivity
         // Declaration
         responseView = (TextView) findViewById(R.id.responseView);
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mHandler = new Handler();
+
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+
+        // load toolbar titles from string resources
+        activityTitles = getResources().getStringArray(R.array.nav_item_activity_titles);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+/*
+        // load nav menu header data
+        loadNavHeader();
+*/
+
+        // initializing navigation menu
+        setUpNavigationView();
+
+        if (savedInstanceState == null) {
+            navItemIndex = 0;
+            CURRENT_TAG = TAG_GAMES;
+            loadHomeFragment();
+        }
 
 
+/*
         Button queryButton = (Button) findViewById(R.id.queryButton);
         queryButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -77,29 +153,227 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+*/
 
     }
 
+    /***
+     * Returns respected fragment that user
+     * selected from navigation menu
+     */
+    private void loadHomeFragment() {
+        // selecting appropriate nav menu item
+        selectNavMenu();
+
+        // set toolbar title
+        setToolbarTitle();
+
+        // if user select the current navigation menu again, don't do anything
+        // just close the navigation drawer
+        if (getSupportFragmentManager().findFragmentByTag(CURRENT_TAG) != null) {
+            drawer.closeDrawers();
+
+            // show or hide the fab button
+            toggleFab();
+            return;
+        }
+
+        // Sometimes, when fragment has huge data, screen seems hanging
+        // when switching between navigation menus
+        // So using runnable, the fragment is loaded with cross fade effect
+        // This effect can be seen in GMail app
+        Runnable mPendingRunnable = new Runnable() {
+            @Override
+            public void run() {
+                // update the main content by replacing fragments
+                Fragment fragment = getHomeFragment();
+                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.setCustomAnimations(android.R.anim.fade_in,
+                        android.R.anim.fade_out);
+                fragmentTransaction.replace(R.id.frame, fragment, CURRENT_TAG);
+                fragmentTransaction.commitAllowingStateLoss();
+            }
+        };
+
+        // If mPendingRunnable is not null, then add to the message queue
+        if (mPendingRunnable != null) {
+            mHandler.post(mPendingRunnable);
+        }
+
+        // show or hide the fab button
+        toggleFab();
+
+        //Closing drawer on item click
+        drawer.closeDrawers();
+
+        // refresh toolbar menu
+        invalidateOptionsMenu();
+    }
+
+    private Fragment getHomeFragment() {
+        switch (navItemIndex) {
+            case 0:
+                // Game frag
+                GamesFragment gameFragment = new GamesFragment();
+                return gameFragment;
+            case 1:
+                // News frag
+                NewsFragment newsFragement = new NewsFragment();
+                return newsFragement;
+            case 2:
+                // Discover frag
+                DiscoverFragment discoverFragment = new DiscoverFragment();
+                return discoverFragment;
+            case 3:
+                // Stations frag
+                StationsFragment stationsFragment = new StationsFragment();
+                return stationsFragment;
+
+            case 4:
+                // Settings frag
+                SettingsFragment settingsFragment = new SettingsFragment();
+                return settingsFragment;
+            default:
+                return new GamesFragment();
+        }
+    }
+
+    private void setToolbarTitle() {
+        getSupportActionBar().setTitle(activityTitles[navItemIndex]);
+    }
+
+    private void selectNavMenu() {
+        navigationView.getMenu().getItem(navItemIndex).setChecked(true);
+    }
+
+    private void setUpNavigationView() {
+        //Setting Navigation View Item Selected Listener to handle the item click of the navigation menu
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+
+            // This method will trigger on item Click of navigation menu
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+
+                //Check to see which item was being clicked and perform appropriate action
+                switch (menuItem.getItemId()) {
+                    //Replacing the main content with ContentFragment Which is our Inbox View;
+                    case R.id.nav_games:
+                        navItemIndex = 0;
+                        CURRENT_TAG = TAG_GAMES;
+                        break;
+                    case R.id.nav_news:
+                        navItemIndex = 1;
+                        CURRENT_TAG = TAG_NEWS;
+                        break;
+                    case R.id.nav_discover:
+                        navItemIndex = 2;
+                        CURRENT_TAG = TAG_DISCOVER;
+                        break;
+                    case R.id.nav_stations:
+                        navItemIndex = 3;
+                        CURRENT_TAG = TAG_STATIONS;
+                        break;
+                    case R.id.nav_settings:
+                        navItemIndex = 4;
+                        CURRENT_TAG = TAG_SETTINGS;
+                        break;
+                    case R.id.nav_share:
+                        // launch new intent instead of loading fragment
+                        startActivity(new Intent(MainActivity.this, ShareActivity.class));
+                        drawer.closeDrawers();
+                        return true;
+                    case R.id.nav_feedback:
+                        // launch new intent instead of loading fragment
+                        startActivity(new Intent(MainActivity.this, FeedbackActivity.class));
+                        drawer.closeDrawers();
+                        return true;
+                    default:
+                        navItemIndex = 0;
+                }
+
+                //Checking if the item is in checked state or not, if not make it in checked state
+                if (menuItem.isChecked()) {
+                    menuItem.setChecked(false);
+                } else {
+                    menuItem.setChecked(true);
+                }
+                menuItem.setChecked(true);
+
+                loadHomeFragment();
+
+                return true;
+            }
+        });
+
+
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.openDrawer, R.string.closeDrawer) {
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                // Code here will be triggered once the drawer closes as we dont want anything to happen so we leave this blank
+                super.onDrawerClosed(drawerView);
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                // Code here will be triggered once the drawer open as we dont want anything to happen so we leave this blank
+                super.onDrawerOpened(drawerView);
+            }
+        };
+
+        //Setting the actionbarToggle to drawer layout
+        drawer.setDrawerListener(actionBarDrawerToggle);
+
+        //calling sync state is necessary or else your hamburger icon wont show up
+        actionBarDrawerToggle.syncState();
+    }
+
+    // show or hide the fab
+    private void toggleFab() {
+        if (navItemIndex == 0)
+            fab.show();
+        else
+            fab.hide();
+    }
+
+
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
+            drawer.closeDrawers();
+            return;
         }
+
+        // This code loads home fragment when back key is pressed
+        // when user is in other fragment than home
+        if (shouldLoadHomeFragOnBackPress) {
+            // checking if user is on other navigation menu
+            // rather than home
+            if (navItemIndex != 0) {
+                navItemIndex = 0;
+                CURRENT_TAG = TAG_GAMES;
+                loadHomeFragment();
+                return;
+            }
+        }
+
+        super.onBackPressed();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
 
-        // Retrieve the SearchView and plug it into SearchManager
-        final SearchView searchView = (SearchView) MenuItemCompat.getActionView(menu.findItem(R.id.action_search));
-        SearchManager searchManager = (SearchManager) getSystemService(SEARCH_SERVICE);
-        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+       if (navItemIndex == 0) {
+           getMenuInflater().inflate(R.menu.main, menu);
+
+           // Retrieve the SearchView and plug it into SearchManager
+           final SearchView searchView = (SearchView) MenuItemCompat.getActionView(menu.findItem(R.id.action_search));
+           SearchManager searchManager = (SearchManager) getSystemService(SEARCH_SERVICE);
+           searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+       }
         return true;
+
     }
 
     @Override
@@ -111,6 +385,8 @@ public class MainActivity extends AppCompatActivity
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Toast.makeText(getApplicationContext(), "Clicked on settings!", Toast.LENGTH_LONG).show();
+
             return true;
         }
 
@@ -141,14 +417,19 @@ public class MainActivity extends AppCompatActivity
         } else if (id == R.id.nav_share) {
             Log.i(TAG, "Clicked on nav_share");
 
-        } else if (id == R.id.nav_send) {
-            Log.i(TAG, "Clicked on nav_send");
+        } else if (id == R.id.nav_feedback) {
+            Log.i(TAG, "Clicked on nav_feedback");
 
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 
     private class RetrieveFeedTask extends AsyncTask<Void, Void, String> {
